395066704fc66da4725a0c5e1cafa6a578e52081
MokshMehndiratta <moksh.mehndiratta@trilogy.com>
2025-07-09 13:13:25 +0530

    update

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -16,6 +16,7 @@ import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
 import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
